6efa5b5238eb6eb59095c06d4f2bb3611bddfa65
Jesstrej <jessica16tor@yahoo.com>
2018-06-27 16:53:30 -0700

    update

---
+++ assets/javascript/logic.js
@@ -0,0 +1,10 @@
+
+var config = {
+    apiKey: "AIzaSyA0Nr6YgKnjUvDsltmDyxs2lnaYac35EIo",
+    authDomain: "train-scheduler-57d67.firebaseapp.com",
+    databaseURL: "https://train-scheduler-57d67.firebaseio.com",
+    projectId: "train-scheduler-57d67",
+    storageBucket: "",
+    messagingSenderId: "845122066171"
+};
+firebase.initializeApp(config);
